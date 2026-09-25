36682206348f82d29365781dbfbf5f87d2758327
ZamCan <zamcannexus@gmail.com>
2026-09-25 20:56:16 +0300

    Add local QR generation for pairing

---
+++ app/src/main/java/com/zamcan/nisaacare/ui/PairingQrCode.kt
@@ -0,0 +1,26 @@
+package com.zamcan.nisaacare.ui
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import com.google.zxing.qrcode.QRCodeWriter
+
+object PairingQrCode {
+    fun create(content: String, size: Int = 720): Bitmap {
+        val hints = mapOf(
+            EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.M,
+            EncodeHintType.MARGIN to 2,
+            EncodeHintType.CHARACTER_SET to "UTF-8"
+        )
+        val matrix = QRCodeWriter().encode(content, BarcodeFormat.QR_CODE, size, size, hints)
+        return Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888).also { bitmap ->
+            for (x in 0 until size) {
+                for (y in 0 until size) {
+                    bitmap.setPixel(x, y, if (matrix.get(x, y)) Color.BLACK else Color.WHITE)
+                }
+            }
+        }
+    }
+}
